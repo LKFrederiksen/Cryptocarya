01122e59bf60d9506a45f8149e364b6574517817
Laura Kragh Frederiksen <laurafrederiksen@Lauras-MacBook-Air-2.local>
2022-09-22 15:00:15 +0200

    - to _

--- workflow.py
+++ workflow.py
@@ -259,7 +259,7 @@ def fastqc_raw(name,path_in ,path_out, done,):
 ########################################################################################################################
 
 #Species removed from pipeline as they had no gene recovery [3050,3188,3272,3300,3316, 3364, 3392, 3394]
-sp = ["Ocotea-foetens-WE521"]
+sp = ["Ocotea-foetens_WE521"]
 # sp = ["Ocotea-foetens-WE521","Ocotea-gabonensis-WE522","Ocotea-meziana-WE523","Pleurothyrium-cuneifolium-WE524","Mespilodaphne-cymbarum-WE525","Damburneya-gentlei-WE526","Ocotea-glaucosericea-WE527","Ocotea-complicata-WE528","Ocotea-javitensis-WE529","Ocotea-skutchii-WE530","Ocotea-sinuata-WE531","Ocotea-botrantha-WE532","Nectandra-lineatifolia-WE533"] 
 
 
