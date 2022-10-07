b767f8cf07d788ddccbf02db458cdf8d3ac374db
Laura Kragh Frederiksen <laurafrederiksen@Lauras-MacBook-Air-2.local>
2022-10-07 18:58:19 +0200

    Test

--- workflow.py
+++ workflow.py
@@ -268,7 +268,7 @@ def paralogs(name, path_in, done, in_done):
     
     conda activate HybPiper
     
-    hybpiper paralog_retriever {name} -t_dna /home/laurakf/cryptocarya/TargetFile/mega353.fasta
+    hybpiper paralog_retriever {path_in}{name} -t_dna /home/laurakf/cryptocarya/TargetFile/mega353.fasta
     
     touch {done}
 
