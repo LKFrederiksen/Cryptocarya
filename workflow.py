da448b2e837eba4b51c9171c8c72b3cb5d4f04f0
Laura Kragh Frederiksen <laurafrederiksen@Lauras-MacBook-Air-2.local>
2022-10-28 15:58:52 +0200

    Test

--- workflow.py
+++ workflow.py
@@ -606,7 +606,7 @@ def optrim(path_in, path_out, done):
  #Going to folder with trimmed files
     cd {path_in}
 
-    R --vanilla /home/laurakf/cryptocarya/Scripts/optrimal.R
+    R /home/laurakf/cryptocarya/Scripts/optrimal.R
 
     touch {done}
 
