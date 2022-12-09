bfe4d6a9066a31789c884933abe4ab602576f4b9
laura <laura.kragh.frederiksen@gmail.com>
2022-12-09 11:49:34 +0100

    Test

--- workflow.py
+++ workflow.py
@@ -498,80 +498,80 @@ gwf = Workflow()
 ### Here you should wait for the output. The output will comprise a file for each gene with the species sequence recovered.
 
 
-##########################################################################################################################
-###############################################---- MAFFT ----#############################################################
-##########################################################################################################################
+# ##########################################################################################################################
+# ###############################################---- MAFFT ----#############################################################
+# ##########################################################################################################################
 
-# Here go to folder 08_Retrieve and ls -1
-# Get the gene names and write them in genes = []
-# We found  genes for Lauraceae using the mega353 target file.
+# # Here go to folder 08_Retrieve and ls -1
+# # Get the gene names and write them in genes = []
+# # We found  genes for Lauraceae using the mega353 target file.
 
-def mafft(genes, path_in, path_out, done, gene):
-    """Aligning all the sequences for each gene."""
-    path_ins = [path_in+genes]
-    outputs = [done, path_out+gene+"_aligned.fasta"] 
-    options = {'cores': 4, 'memory': "4g", 'walltime': "1:00:00", 'account':"cryptocarya"}
+# def mafft(genes, path_in, path_out, done, gene):
+#     """Aligning all the sequences for each gene."""
+#     path_ins = [path_in+genes]
+#     outputs = [done, path_out+gene+"_aligned.fasta"] 
+#     options = {'cores': 4, 'memory': "4g", 'walltime': "1:00:00", 'account':"cryptocarya"}
 
-    spec = """
+#     spec = """
 
-    source /home/laurakf/miniconda3/etc/profile.d/conda.sh
-
-    conda activate Mafft
-
-    cd {path_in}
-
-    mafft --thread 4 --globalpair --adjustdirectionaccurately --maxiterate 1000 {genes} > {path_out}{gene}_aligned.fasta
-
-    echo touching {done}
-
-    touch {done}
-
-    """.format(genes = genes, done = done, path_in = path_in, path_out = path_out, gene = gene)
-
-    return (path_ins, outputs, options, spec)
-    
-#It is a good idea to rename the fasta files here.
-
-
-# ########################################################################################################################
-# ###############################################---- TRIMAL ----#########################################################
-# ########################################################################################################################
-
-# #Cleaning according trimal
-# #Get raw alignments and trim them according to a gap threshold.
-
-# # Make sure to adjust path in script 'gap_trimming.sh'
-# # Seems like it is necessary to rename the *.fasta to *.fasta.old yourself. Otherwise perhaps my path was wrong?
-# # find . -depth -name "*.fasta" -exec sh -c 'f="{}"; mv -- "$f" "${f%.fasta}.fasta.old"' \;
-# # This way it did take the *.old files and move them to each directory 0.1 etc. and trim them. 
-
-# def gt_trimming(path_in, path_out, done, gene):
-#     """ Use trimal for trimming all alignments for each of the GT values specified"""
-#     inputs = [path_in+gene+"_aligned.fasta"]
-#     outputs = [done]
-#     options = {'cores': 1, 'memory': "5g", 'walltime': "0:20:00", 'account':"cryptocarya"}
-
-#     spec="""
-    
-#     #Activating Trimal
 #     source /home/laurakf/miniconda3/etc/profile.d/conda.sh
-#     conda activate Trimal
 
-#     # Moved alignments to Trimal folder
-#     #Go to alignments folder 
+#     conda activate Mafft
+
 #     cd {path_in}
 
-#     #Running gaptrimming.sh
-#     cd {path_out}
-#     bash /home/laurakf/cryptocarya/Scripts/gap_trimming.sh -g {gene}_aligned.fasta.old
+#     mafft --thread 4 --globalpair --adjustdirectionaccurately --maxiterate 1000 {genes} > {path_out}{gene}_aligned.fasta
 
 #     echo touching {done}
 
 #     touch {done}
 
-#     """.format(path_in = path_in, done = done, gene = gene, path_out = path_out)
+#     """.format(genes = genes, done = done, path_in = path_in, path_out = path_out, gene = gene)
 
-#     return(inputs, outputs, options, spec)
+#     return (path_ins, outputs, options, spec)
+    
+# #It is a good idea to rename the fasta files here.
+
+
+########################################################################################################################
+###############################################---- TRIMAL ----#########################################################
+########################################################################################################################
+
+#Cleaning according trimal
+#Get raw alignments and trim them according to a gap threshold.
+
+# Make sure to adjust path in script 'gap_trimming.sh'
+# Seems like it is necessary to rename the *.fasta to *.fasta.old yourself. Otherwise perhaps my path was wrong?
+# find . -depth -name "*.fasta" -exec sh -c 'f="{}"; mv -- "$f" "${f%.fasta}.fasta.old"' \;
+# This way it did take the *.old files and move them to each directory 0.1 etc. and trim them. 
+
+def gt_trimming(path_in, path_out, done, gene):
+    """ Use trimal for trimming all alignments for each of the GT values specified"""
+    inputs = [path_in+gene+"_aligned.fasta"]
+    outputs = [done]
+    options = {'cores': 1, 'memory': "5g", 'walltime': "0:20:00", 'account':"cryptocarya"}
+
+    spec="""
+    
+    #Activating Trimal
+    source /home/laurakf/miniconda3/etc/profile.d/conda.sh
+    conda activate Trimal
+
+    # Moved alignments to Trimal folder
+    #Go to alignments folder 
+    cd {path_in}
+
+    #Running gaptrimming.sh
+    cd {path_out}
+    bash /home/laurakf/cryptocarya/Scripts/gap_trimming.sh -g {gene}_aligned.fasta.old
+
+    echo touching {done}
+
+    touch {done}
+
+    """.format(path_in = path_in, done = done, gene = gene, path_out = path_out)
+
+    return(inputs, outputs, options, spec)
     
     
 #######################################################################################################################
@@ -1150,21 +1150,21 @@ gene = ["4471", "4527", "4691", "4724", "4744", "4757", "4793", "4796", "4802", 
 genes = ["4471.FNA", "4527.FNA", "4691.FNA", "4724.FNA", "4744.FNA", "4757.FNA", "4793.FNA", "4796.FNA", "4802.FNA", "4806.FNA", "4848.FNA", "4889.FNA", "4890.FNA", "4893.FNA", "4932.FNA", "4942.FNA", "4951.FNA", "4954.FNA", "4992.FNA", "5018.FNA", "5032.FNA", "5034.FNA", "5038.FNA", "5090.FNA", "5104.FNA", "5116.FNA", "5123.FNA", "5131.FNA", "5138.FNA", "5162.FNA", "5163.FNA", "5168.FNA", "5177.FNA", "5188.FNA", "5200.FNA", "5206.FNA", "5220.FNA", "5257.FNA", "5264.FNA", "5271.FNA", "5273.FNA", "5280.FNA", "5296.FNA", "5299.FNA", "5304.FNA", "5318.FNA", "5326.FNA", "5328.FNA", "5333.FNA", "5335.FNA", "5339.FNA", "5343.FNA", "5348.FNA", "5354.FNA", "5366.FNA", "5398.FNA", "5404.FNA", "5406.FNA", "5421.FNA", "5426.FNA", "5427.FNA", "5428.FNA", "5449.FNA", "5454.FNA", "5460.FNA", "5463.FNA", "5464.FNA", "5469.FNA", "5477.FNA", "5489.FNA", "5502.FNA", "5513.FNA", "5528.FNA", "5531.FNA", "5536.FNA", "5551.FNA", "5554.FNA", "5562.FNA", "5578.FNA", "5594.FNA", "5596.FNA", "5599.FNA", "5614.FNA", "5620.FNA", "5634.FNA", "5639.FNA", "5644.FNA", "5656.FNA", "5660.FNA", "5664.FNA", "5670.FNA", "5699.FNA", "5702.FNA", "5703.FNA", "5716.FNA", "5721.FNA", "5744.FNA", "5770.FNA", "5772.FNA", "5791.FNA", "5802.FNA", "5815.FNA", "5816.FNA", "5821.FNA", "5822.FNA", "5840.FNA", "5841.FNA", "5842.FNA", "5843.FNA", "5849.FNA", "5853.FNA", "5857.FNA", "5858.FNA", "5865.FNA", "5866.FNA", "5870.FNA", "5893.FNA", "5894.FNA", "5899.FNA", "5910.FNA", "5913.FNA", "5918.FNA", "5919.FNA", "5921.FNA", "5926.FNA", "5933.FNA", "5942.FNA", "5943.FNA", "5944.FNA", "5945.FNA", "5949.FNA", "5950.FNA", "5960.FNA", "5968.FNA", "5974.FNA", "5977.FNA", "5980.FNA", "5981.FNA", "5990.FNA", "6000.FNA", "6003.FNA", "6004.FNA", "6016.FNA", "6026.FNA", "6029.FNA", "6034.FNA", "6036.FNA", "6038.FNA", "6041.FNA", "6048.FNA", "6050.FNA", "6051.FNA", "6056.FNA", "6064.FNA", "6068.FNA", "6072.FNA", "6098.FNA", "6114.FNA", "6119.FNA", "6128.FNA", "6130.FNA", "6139.FNA", "6148.FNA", "6164.FNA", "6175.FNA", "6176.FNA", "6198.FNA", "6216.FNA", "6221.FNA", "6226.FNA", "6227.FNA", "6238.FNA", "6258.FNA", "6265.FNA", "6274.FNA", "6282.FNA", "6284.FNA", "6295.FNA", "6298.FNA", "6299.FNA", "6303.FNA", "6318.FNA", "6320.FNA", "6363.FNA", "6366.FNA", "6373.FNA", "6376.FNA", "6378.FNA", "6383.FNA", "6384.FNA", "6389.FNA", "6393.FNA", "6398.FNA", "6404.FNA", "6405.FNA", "6406.FNA", "6407.FNA", "6412.FNA", "6420.FNA", "6432.FNA", "6439.FNA", "6447.FNA", "6450.FNA", "6454.FNA", "6457.FNA", "6458.FNA", "6459.FNA", "6460.FNA", "6462.FNA", "6483.FNA", "6487.FNA", "6488.FNA", "6492.FNA", "6494.FNA", "6496.FNA", "6500.FNA", "6506.FNA", "6507.FNA", "6526.FNA", "6527.FNA", "6528.FNA", "6532.FNA", "6533.FNA", "6538.FNA", "6540.FNA", "6544.FNA", "6550.FNA", "6552.FNA", "6559.FNA", "6563.FNA", "6570.FNA", "6572.FNA", "6601.FNA", "6620.FNA", "6631.FNA", "6636.FNA", "6639.FNA", "6641.FNA", "6649.FNA", "6652.FNA", "6660.FNA", "6667.FNA", "6685.FNA", "6689.FNA", "6713.FNA", "6717.FNA", "6732.FNA", "6733.FNA", "6738.FNA", "6746.FNA", "6779.FNA", "6782.FNA", "6785.FNA", "6792.FNA", "6797.FNA", "6825.FNA", "6848.FNA", "6854.FNA", "6859.FNA", "6860.FNA", "6865.FNA", "6875.FNA", "6882.FNA", "6883.FNA", "6909.FNA", "6913.FNA", "6914.FNA", "6924.FNA", "6933.FNA", "6946.FNA", "6947.FNA", "6954.FNA", "6958.FNA", "6961.FNA", "6962.FNA", "6968.FNA", "6978.FNA", "6979.FNA", "6992.FNA", "7021.FNA", "7024.FNA", "7029.FNA", "7067.FNA", "7111.FNA", "7128.FNA", "7135.FNA", "7136.FNA", "7141.FNA", "7174.FNA", "7194.FNA", "7241.FNA", "7273.FNA", "7279.FNA", "7313.FNA", "7324.FNA", "7325.FNA", "7331.FNA", "7333.FNA", "7336.FNA", "7363.FNA", "7367.FNA", "7371.FNA", "7572.FNA", "7577.FNA", "7583.FNA", "7602.FNA", "7628.FNA"]
 
 
-#### MAFFT
-for i in range(len(genes)):
-    gwf.target_from_template('Mafft_'+genes[i], mafft(genes = genes[i],
-                                                        path_out= "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/09_Mafft/",
-                                                        path_in = "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/08_Retrieve/",
-                                                        gene = gene[i],
-                                                        done = "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/09_Mafft/done/"+genes[i]))
+# #### MAFFT
+# for i in range(len(genes)):
+#     gwf.target_from_template('Mafft_'+genes[i], mafft(genes = genes[i],
+#                                                         path_out= "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/09_Mafft/",
+#                                                         path_in = "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/08_Retrieve/",
+#                                                         gene = gene[i],
+#                                                         done = "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/09_Mafft/done/"+genes[i]))
 
 
-# #### Trimal according to a pre-defined gt values
-# for i in range(len(gene)):
-#    gwf.target_from_template('gt_trimming_'+gene[i], gt_trimming(gene = gene[i],
-#                                                         path_in = "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/09_Mafft/",
-#                                                         path_out = "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/10_Trimal/",
-#                                                         done = "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/10_Trimal/done/"+gene[i]))
+#### Trimal according to a pre-defined gt values
+for i in range(len(gene)):
+   gwf.target_from_template('gt_trimming_'+gene[i], gt_trimming(gene = gene[i],
+                                                        path_in = "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/09_Mafft/",
+                                                        path_out = "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/10_Trimal/",
+                                                        done = "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/10_Trimal/done/"+gene[i]))
 
 # ### Generating AMAS statistics for raw_alignments
 # gwf.target_from_template('amas_raw', amas_raw(path_in = "/home/laurakf/cryptocarya/Workflow/PAFTOL-exons/10_Trimal",
