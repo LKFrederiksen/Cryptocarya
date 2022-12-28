47b96ca764cdc638bca8cf611c65adf8a4fb6e6e
laura <laura.kragh.frederiksen@gmail.com>
2022-12-28 19:07:08 +0100

    Test

--- workflow.py
+++ workflow.py
@@ -401,11 +401,11 @@ def coverageOutgroup(name, path_in, path_out, done,all_bam,all_sorted_bam, all_s
     # Added this step to concatenate files after combining them in the coverage script. 
     # Remove this step when running the remaining part of the coverage script.
     
-    # cd {path_out}
-    # cat {name}.fasta_supercontig {name}.fasta_exon > {name}.fasta
+    cd {path_out}
+    cat {name}.fasta_supercontig {name}.fasta_exon > {name}.fasta
 
-    # rm *.fasta_supercontig
-    # rm *.fasta_exon
+    rm *.fasta_supercontig
+    rm *.fasta_exon
     
     echo touching {done}
 
