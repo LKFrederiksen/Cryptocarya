36d582e1340f9358bfb11cbbfafc667661da9f48
laura <laura.kragh.frederiksen@gmail.com>
2022-12-31 15:18:53 +0100

    Test

--- workflow.py
+++ workflow.py
@@ -769,81 +769,81 @@ gwf = Workflow()
 #     return(inputs, outputs, options, spec)
 
 
-##########################################################################################################################
-##########################---- Copying alignments and creating partitions ----############################################
-##########################################################################################################################
+# ##########################################################################################################################
+# ##########################---- Copying alignments and creating partitions ----############################################
+# ##########################################################################################################################
 
-def partitioner(path_in, path_out, gene, done):
-    """Copying alignments from the manual alignment folder to the treebuilding folder and creating partition files"""
-    inputs = [path_in+gene+"_output_taper.fasta"]
-    outputs = [path_out+gene+"_part.txt",path_out+gene+"_clean.fasta", done]
-    options = {'cores': 1, 'memory': "5g", 'walltime': "00:20:00", 'account':"cryptocarya"}
-
-    spec = """
-
-    source /home/laurakf/miniconda3/etc/profile.d/conda.sh
-    conda activate HybPiper
-
-
-    #Going to folder with data
-    cd {path_in}
-    
-    #The partitioner should produce 2 files for each gene
-    #one file called gene_aligned_part.txt which is the partitioning file
-    #another called gene_aligned_clean.fasta which are just the sequences without the exons
-
-    python3 /home/laurakf/cryptocarya/Scripts/partitioner.py --smoother 10 --gene {gene} --file_ending _output_taper.fasta
-        
-    #Moving files to the correct folders
-    mv {gene}_clean.fasta {path_out}
-    mv {gene}_part.txt {path_out} 
-
-    touch {done}
-
-
-    #Partitioning skal køres på alle gener med exon og supercontig.
-    """.format(path_in = path_in, gene = gene, done = done, path_out = path_out)
-
-    return (inputs, outputs, options, spec)
-
-
-# ###########################################################################################################################
-# #############################################---- IQ-tree ----#############################################################
-# ###########################################################################################################################
-
-# def iq_tree(path_in, gene,path_out, done):
-#     """Using Iq-tree to produce trees for each gene with a partition file to use individual substitution rates for each gene"""
-#     inputs = [path_in+gene+"_part.txt", path_in+gene+"_clean.fasta"]
-#     outputs = [path_out+gene+".txt.tre"]
-#     options = {'cores': 10, 'memory': "10g", 'walltime': "01:00:00", 'account':"cryptocarya"}
+# def partitioner(path_in, path_out, gene, done):
+#     """Copying alignments from the manual alignment folder to the treebuilding folder and creating partition files"""
+#     inputs = [path_in+gene+"_output_taper.fasta"]
+#     outputs = [path_out+gene+"_part.txt",path_out+gene+"_clean.fasta", done]
+#     options = {'cores': 1, 'memory': "5g", 'walltime': "00:20:00", 'account':"cryptocarya"}
 
 #     spec = """
 
-
-#     # Activate IQtree    
 #     source /home/laurakf/miniconda3/etc/profile.d/conda.sh
-#     conda activate IQtree
+#     conda activate HybPiper
 
+
+#     #Going to folder with data
 #     cd {path_in}
+    
+#     #The partitioner should produce 2 files for each gene
+#     #one file called gene_aligned_part.txt which is the partitioning file
+#     #another called gene_aligned_clean.fasta which are just the sequences without the exons
 
-#     #Actual IQtree tree search. 
-#     iqtree2 -s {gene}_clean.fasta -p {gene}_part.txt -T AUTO -ntmax 20 -m MFP -B 1000 -redo 
-
-#     mv {gene}*.treefile {path_out}
-#     mv {gene}*.model.gz {path_out}
-#     mv {gene}*.contree {path_out}
-#     mv {gene}*.bionj {path_out}
-#     mv {gene}*.ckp.gz {path_out}
-#     mv {gene}*.iqtree {path_out}
-#     mv {gene}*.log {path_out}
-#     mv {gene}*.mldist {path_out}
-#     mv {gene}*.splits.nex {path_out}
+#     python3 /home/laurakf/cryptocarya/Scripts/partitioner.py --smoother 10 --gene {gene} --file_ending _output_taper.fasta
         
+#     #Moving files to the correct folders
+#     mv {gene}_clean.fasta {path_out}
+#     mv {gene}_part.txt {path_out} 
+
 #     touch {done}
 
-#     """.format(path_in = path_in, gene = gene, path_out=path_out, done = done)
+
+#     #Partitioning skal køres på alle gener med exon og supercontig.
+#     """.format(path_in = path_in, gene = gene, done = done, path_out = path_out)
 
 #     return (inputs, outputs, options, spec)
+
+
+###########################################################################################################################
+#############################################---- IQ-tree ----#############################################################
+###########################################################################################################################
+
+def iq_tree(path_in, gene,path_out, done):
+    """Using Iq-tree to produce trees for each gene with a partition file to use individual substitution rates for each gene"""
+    inputs = [path_in+gene+"_part.txt", path_in+gene+"_clean.fasta"]
+    outputs = [path_out+gene+".txt.tre"]
+    options = {'cores': 10, 'memory': "10g", 'walltime': "01:00:00", 'account':"cryptocarya"}
+
+    spec = """
+
+
+    # Activate IQtree    
+    source /home/laurakf/miniconda3/etc/profile.d/conda.sh
+    conda activate IQtree
+
+    cd {path_in}
+
+    #Actual IQtree tree search. 
+    iqtree2 -s {gene}_clean.fasta -p {gene}_part.txt -T AUTO -ntmax 20 -m MFP -B 1000 -redo 
+
+    mv {gene}*.treefile {path_out}
+    mv {gene}*.model.gz {path_out}
+    mv {gene}*.contree {path_out}
+    mv {gene}*.bionj {path_out}
+    mv {gene}*.ckp.gz {path_out}
+    mv {gene}*.iqtree {path_out}
+    mv {gene}*.log {path_out}
+    mv {gene}*.mldist {path_out}
+    mv {gene}*.splits.nex {path_out}
+        
+    touch {done}
+
+    """.format(path_in = path_in, gene = gene, path_out=path_out, done = done)
+
+    return (inputs, outputs, options, spec)
 
 
 # ########################################################################################################################
@@ -1332,25 +1332,25 @@ def partitioner(path_in, path_out, gene, done):
     #                                                     done = "/home/laurakf/cryptocarya/Workflow/PAFTOL-comb/14_mapping/done/"+gene[i]))
 
 
+# gene = ["4471", "4527", "4691", "4724", "4744", "4757", "4793", "4796", "4802", "4806", "4848", "4889", "4890", "4893", "4932", "4942", "4951", "4954", "4989", "4992", "5018", "5032", "5034", "5038", "5064", "5090", "5104", "5116", "5123", "5131", "5138", "5162", "5168", "5177", "5188", "5200", "5206", "5220", "5257", "5260", "5264", "5271", "5273", "5280", "5296", "5299", "5304", "5318", "5326", "5328", "5333", "5335", "5339", "5343", "5347", "5348", "5354", "5355", "5357", "5366", "5398", "5404", "5406", "5421", "5426", "5428", "5430", "5434", "5449", "5454", "5460", "5463", "5464", "5469", "5477", "5489", "5502", "5513", "5528", "5531", "5536", "5551", "5554", "5562", "5578", "5594", "5596", "5614", "5620", "5634", "5639", "5642", "5644", "5656", "5660", "5664", "5670", "5699", "5702", "5703", "5716", "5721", "5733", "5744", "5770", "5772", "5791", "5802", "5815", "5816", "5821", "5822", "5840", "5841", "5842", "5843", "5849", "5853", "5857", "5858", "5859", "5865", "5866", "5870", "5893", "5894", "5899", "5910", "5913", "5918", "5919", "5922", "5926", "5933", "5936", "5940", "5941", "5942", "5944", "5945", "5949", "5950", "5958", "5960", "5968", "5974", "5977", "5980", "5981", "5990", "6000", "6003", "6004", "6016", "6026", "6029", "6034", "6036", "6038", "6048", "6050", "6051", "6056", "6064", "6068", "6072", "6098", "6110", "6114", "6119", "6128", "6130", "6139", "6148", "6150", "6164", "6175", "6176", "6198", "6216", "6221", "6226", "6227", "6238", "6258", "6265", "6270", "6274", "6282", "6284", "6295", "6298", "6299", "6303", "6318", "6320", "6363", "6366", "6373", "6376", "6378", "6379", "6383", "6384", "6387", "6389", "6393", "6398", "6404", "6405", "6406", "6407", "6412", "6420", "6430", "6432", "6439", "6447", "6448", "6449", "6450", "6454", "6457", "6458", "6459", "6460", "6462", "6483", "6487", "6488", "6492", "6494", "6496", "6498", "6500", "6506", "6507", "6526", "6527", "6528", "6532", "6533", "6538", "6540", "6544", "6550", "6552", "6557", "6559", "6563", "6565", "6570", "6572", "6601", "6620", "6631", "6636", "6639", "6641", "6649", "6652", "6660", "6667", "6679", "6685", "6689", "6705", "6713", "6717", "6732", "6733", "6738", "6746", "6779", "6780", "6782", "6785", "6791", "6792", "6797", "6825", "6848", "6854", "6859", "6860", "6864", "6865", "6875", "6882", "6883", "6886", "6893", "6909", "6913", "6914", "6924", "6933", "6946", "6947", "6954", "6955", "6958", "6961", "6962", "6968", "6969", "6977", "6978", "6979", "6992", "6995", "7013", "7021", "7024", "7028", "7029", "7067", "7111", "7128", "7135", "7136", "7141", "7174", "7194", "7241", "7273", "7279", "7296", "7313", "7324", "7325", "7331", "7333", "7336", "7361", "7363", "7367", "7371", "7577", "7583", "7602", "7628"]
+
+# for i in range(0, len(gene)):
+#    ### Creating the partition files for each gene
+#     gwf.target_from_template('Partition_'+gene[i], partitioner(gene = gene[i],
+#                                                         path_in = "/home/laurakf/cryptocarya/Workflow/PAFTOL-comb/14_mapping/",
+#                                                         path_out= "/home/laurakf/cryptocarya/Workflow/PAFTOL-comb/15_partitions/",
+#                                                         done = "/home/laurakf/cryptocarya/Workflow/PAFTOL-comb/15_partitions/done/"+gene[i]))
+
+
 gene = ["4471", "4527", "4691", "4724", "4744", "4757", "4793", "4796", "4802", "4806", "4848", "4889", "4890", "4893", "4932", "4942", "4951", "4954", "4989", "4992", "5018", "5032", "5034", "5038", "5064", "5090", "5104", "5116", "5123", "5131", "5138", "5162", "5168", "5177", "5188", "5200", "5206", "5220", "5257", "5260", "5264", "5271", "5273", "5280", "5296", "5299", "5304", "5318", "5326", "5328", "5333", "5335", "5339", "5343", "5347", "5348", "5354", "5355", "5357", "5366", "5398", "5404", "5406", "5421", "5426", "5428", "5430", "5434", "5449", "5454", "5460", "5463", "5464", "5469", "5477", "5489", "5502", "5513", "5528", "5531", "5536", "5551", "5554", "5562", "5578", "5594", "5596", "5614", "5620", "5634", "5639", "5642", "5644", "5656", "5660", "5664", "5670", "5699", "5702", "5703", "5716", "5721", "5733", "5744", "5770", "5772", "5791", "5802", "5815", "5816", "5821", "5822", "5840", "5841", "5842", "5843", "5849", "5853", "5857", "5858", "5859", "5865", "5866", "5870", "5893", "5894", "5899", "5910", "5913", "5918", "5919", "5922", "5926", "5933", "5936", "5940", "5941", "5942", "5944", "5945", "5949", "5950", "5958", "5960", "5968", "5974", "5977", "5980", "5981", "5990", "6000", "6003", "6004", "6016", "6026", "6029", "6034", "6036", "6038", "6048", "6050", "6051", "6056", "6064", "6068", "6072", "6098", "6110", "6114", "6119", "6128", "6130", "6139", "6148", "6150", "6164", "6175", "6176", "6198", "6216", "6221", "6226", "6227", "6238", "6258", "6265", "6270", "6274", "6282", "6284", "6295", "6298", "6299", "6303", "6318", "6320", "6363", "6366", "6373", "6376", "6378", "6379", "6383", "6384", "6387", "6389", "6393", "6398", "6404", "6405", "6406", "6407", "6412", "6420", "6430", "6432", "6439", "6447", "6448", "6449", "6450", "6454", "6457", "6458", "6459", "6460", "6462", "6483", "6487", "6488", "6492", "6494", "6496", "6498", "6500", "6506", "6507", "6526", "6527", "6528", "6532", "6533", "6538", "6540", "6544", "6550", "6552", "6557", "6559", "6563", "6565", "6570", "6572", "6601", "6620", "6631", "6636", "6639", "6641", "6649", "6652", "6660", "6667", "6679", "6685", "6689", "6705", "6713", "6717", "6732", "6733", "6738", "6746", "6779", "6780", "6782", "6785", "6791", "6792", "6797", "6825", "6848", "6854", "6859", "6860", "6864", "6865", "6875", "6882", "6883", "6886", "6893", "6909", "6913", "6914", "6924", "6933", "6946", "6947", "6954", "6955", "6958", "6961", "6962", "6968", "6969", "6977", "6978", "6979", "6992", "6995", "7013", "7021", "7024", "7028", "7029", "7067", "7111", "7128", "7135", "7136", "7141", "7174", "7194", "7241", "7273", "7279", "7296", "7313", "7324", "7325", "7331", "7333", "7336", "7361", "7363", "7367", "7371", "7577", "7583", "7602", "7628"]
 
+
+#Running IQTREE for files trimmed with trimal and CIAlign                                             
 for i in range(0, len(gene)):
-   ### Creating the partition files for each gene
-    gwf.target_from_template('Partition_'+gene[i], partitioner(gene = gene[i],
-                                                        path_in = "/home/laurakf/cryptocarya/Workflow/PAFTOL-comb/14_mapping/",
-                                                        path_out= "/home/laurakf/cryptocarya/Workflow/PAFTOL-comb/15_partitions/",
-                                                        done = "/home/laurakf/cryptocarya/Workflow/PAFTOL-comb/15_partitions/done/"+gene[i]))
-
-
-# gene = ["4471", "4527", "4691", "4724", "4744", "4757", "4793", "4796", "4802", "4806", "4848", "4889", "4890", "4893", "4932", "4942", "4951", "4954", "4992", "5018", "5032", "5034", "5038", "5090", "5104", "5116", "5123", "5131", "5138", "5162", "5163", "5168", "5177", "5188", "5200", "5206", "5220", "5257", "5264", "5271", "5273", "5280", "5296", "5299", "5304", "5318", "5326", "5328", "5333", "5335", "5339", "5343", "5348", "5354", "5366", "5398", "5404", "5406", "5421", "5426", "5427", "5428", "5449", "5454", "5460", "5463", "5464", "5469", "5477", "5489", "5502", "5513", "5528", "5531", "5536", "5551", "5554", "5562", "5578", "5594", "5596", "5614", "5620", "5634", "5639", "5644", "5656", "5660", "5664", "5670", "5699", "5702", "5703", "5716", "5721", "5744", "5770", "5772", "5791", "5802", "5815", "5816", "5821", "5822", "5840", "5841", "5842", "5843", "5849", "5853", "5857", "5858", "5865", "5866", "5870", "5893", "5894", "5899", "5910", "5913", "5918", "5919", "5926", "5933", "5942", "5944", "5945", "5949", "5950", "5960", "5968", "5974", "5977", "5980", "5981", "5990", "6000", "6003", "6004", "6016", "6026", "6029", "6034", "6036", "6038", "6048", "6050", "6051", "6056", "6064", "6068", "6072", "6098", "6114", "6119", "6128", "6130", "6139", "6148", "6164", "6175", "6176", "6198", "6216", "6221", "6226", "6227", "6238", "6258", "6265", "6274", "6282", "6284", "6295", "6298", "6299", "6303", "6318", "6320", "6363", "6366", "6373", "6376", "6378", "6383", "6384", "6389", "6393", "6398", "6404", "6405", "6406", "6407", "6412", "6420", "6432", "6439", "6447", "6450", "6454", "6457", "6458", "6459", "6460", "6462", "6483", "6487", "6488", "6492", "6494", "6496", "6500", "6506", "6507", "6526", "6527", "6528", "6532", "6533", "6538", "6540", "6544", "6550", "6552", "6559", "6563", "6570", "6572", "6601", "6620", "6631", "6636", "6639", "6641", "6649", "6652", "6660", "6667", "6685", "6689", "6713", "6717", "6732", "6733", "6738", "6746", "6779", "6782", "6785", "6792", "6797", "6825", "6848", "6854", "6859", "6860", "6865", "6875", "6882", "6883", "6909", "6913", "6914", "6924", "6933", "6946", "6947", "6954", "6958", "6961", "6962", "6968", "6978", "6979", "6992", "7021", "7024", "7029", "7067", "7111", "7128", "7135", "7136", "7141", "7174", "7194", "7241", "7273", "7279", "7313", "7324", "7325", "7331", "7333", "7336", "7363", "7367", "7371", "7572", "7577", "7583", "7602", "7628"]                                            
-
-
-# #Running IQTREE for files trimmed with trimal and CIAlign                                             
-# for i in range(0, len(gene)):
-#    gwf.target_from_template('Iqtree_'+gene[i], iq_tree(gene = gene[i],
-#                                                     path_out = "/home/laurakf/cryptocarya/Workflow/PAFTOL-partitions/14_IQtree/",
-#                                                     done = "/home/laurakf/cryptocarya/Workflow/PAFTOL-partitions/14_IQtree/done/"+gene[i],
-#                                                     path_in = "/home/laurakf/cryptocarya/Workflow/PAFTOL-partitions/18_partitions/"))  
+   gwf.target_from_template('Iqtree_'+gene[i], iq_tree(gene = gene[i],
+                                                    path_out = "/home/laurakf/cryptocarya/Workflow/PAFTOL-comb/16_IQtree/",
+                                                    done = "/home/laurakf/cryptocarya/Workflow/PAFTOL-comb/16_IQtree/done/"+gene[i],
+                                                    path_in = "/home/laurakf/cryptocarya/Workflow/PAFTOL-comb/15_partitions/"))  
 
 
 # # Running ASTRAL 
